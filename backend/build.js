16fc2a9d0469b65d9c604fd1c56a8d8f023d3058
teja939052 <teja939052@users.noreply.github.com>
2026-09-01 21:30:15 +0530

    fix: use platform-correct tsc binary in build.js

--- backend/build.js
+++ backend/build.js
@@ -1,9 +1,10 @@
 #!/usr/bin/env node
 const { spawn } = require('child_process');
 const path = require('path');
+const os = require('os');
 
-const tsc = path.resolve(__dirname, 'node_modules', '.bin', 'tsc.cmd');
+const tscBin = path.resolve(__dirname, 'node_modules', '.bin', os.platform() === 'win32' ? 'tsc.cmd' : 'tsc');
 const args = ['--project', path.resolve(__dirname, 'tsconfig.json')];
 
-const child = spawn(tsc, args, { stdio: 'inherit', shell: true });
+const child = spawn(tscBin, args, { stdio: 'inherit', shell: true });
 child.on('exit', (code) => process.exit(code));
